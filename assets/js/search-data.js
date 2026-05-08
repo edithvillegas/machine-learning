2bca1be66f197a686cf5069288e85dfe4f4865e8
edithvillegas <29732178+edithvillegas@users.noreply.github.com>
2026-05-08 15:40:55 +0000

    Deploying to gh-pages from @ edithvillegas/machine-learning@5ddf5b1203c8577c4b437f336b896441b386dc8b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,20 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/machine-learning/ubicacion/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/machine-learning/cv/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/machine-learning/people/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
